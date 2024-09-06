d84c0169f976fc4ec9e151fbd5c050d733c531f3
Asbar Roufaida <159757667+Roufaida-As@users.noreply.github.com>
2024-09-07 00:01:12 +0100

    Update productRouter.js

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const productRoute = express.Router();
-const moviesController = require('./../controllers/productsController')
+const productsController = require('./../controllers/productsController')
 
 
 //for all products
 productRoute.route('/')
-    .get(moviesController.getAllProducts)
+    .get(productsController.getAllProducts)
 
 
 //for each product /products/id
 productRoute.route('/:id')
-    .get(moviesController.getProduct)
+    .get(productsController.getProduct)
 
 module.exports = productRoute
