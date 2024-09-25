3a3a2ed32ebceff601c3705e7374394bc7f33bc8
Roufaida-As <r.asbar@esi-sba.dz>
2024-09-25 21:40:41 +0100

    update front

--- client/src/components/product_details/productImage.jsx
+++ client/src/components/product_details/productImage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './productImage.css';
+import API_URL from '../../config';'./../../config'
 
 const ProductImage = ({ image }) => {
     return (
         <div className="product-image-container">
-            <img src={`http://localhost:3002${image}`}  alt="Product" className="product-image" />
+            <img src={`${API_URL}${image}`}  alt="Product" className="product-image" />
         </div>
     );
 }
